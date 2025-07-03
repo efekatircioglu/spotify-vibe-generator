cc54bfdf6d5a3b10fe39863b180b0b82b251c2b0
Efe Katircioglu <122750090+efekatircioglu@users.noreply.github.com>
2025-07-03 14:10:22 +0300

    Add files via upload

---
+++ c/src/app/_app.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp; 
